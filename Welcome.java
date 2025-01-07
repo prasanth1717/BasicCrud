7cccfb951cc4c197981d52dc287d5e71c2de53be
prasanth1717 <prasanthprasanth9482@gmail.com>
2025-01-07 15:02:46 +0530

    Add Welcome class and update README with project introduction

---
+++ Welcome.java
@@ -0,0 +1,14 @@
+public class Welcome {
+
+    public int add(int a, int b) {
+            return a + b;
+        }
+    
+        public static void main(String[] args) {
+            Welcome v1=new Welcome(); 
+            v1.add(10,20);
+
+        System.out.println("Welcome to Basic CRUD Operations");
+    }
+
+}
